78e75eee92e2d828cf3fe5b1e61cfd5280e61496
cvancefrance <cvancefrance@gmail.com>
2025-01-20 14:32:32 -0600

    added LeafNode and started ParentNode

---
+++ src/parentnode.py
@@ -0,0 +1,1 @@
+class ParentNode(HTMLNode):
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_empty(self):
@@ -15,6 +16,28 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple_props(self):
         node = HTMLNode(props={"href": "https://google.com","target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
+
+
+
+
+
+    def test_leaf_node(self):
+    # test basic tag with value
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    # test with props
+        node_with_props = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node_with_props.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+    # test with no tag
+        text_node = LeafNode(None, "Just some text")
+        self.assertEqual(text_node.to_html(), "Just some text")
+
+    # test with no value (should raise ValueError)
+        with self.assertRaises(ValueError):
+            invalid_node = LeafNode("p", None)
+            invalid_node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
