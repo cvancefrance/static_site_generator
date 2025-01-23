30b36e795f6da0d1665e9906c6d33eecbf9c1953
cvancefrance <cvancefrance@gmail.com>
2025-01-22 21:31:01 -0600

    added split_node images and links, and tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,5 +1,5 @@
 from textnode import TextType, TextNode
-
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -31,3 +31,59 @@ def find_delimiter_pairs(text, delimiter):
           if end == -1:
                return False
           index = end + len(delimiter)
+
+
+
+
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            images = extract_markdown_images(node.text)
+            if not images:
+                new_nodes.append(node)
+            else:
+                text = node.text
+                for alt_text, url in images:
+                    sections = text.split(f"![{alt_text}]({url})", 1)
+                    if sections[0]:
+                        new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                    new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                    text = sections[1]
+
+                if text:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
+
+
+
+
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            links = extract_markdown_links(node.text)
+            if not links:
+                new_nodes.append(node)
+            else:
+                text = node.text
+                for link, url in links:
+                    sections = text.split(f"[{link}]({url})", 1)
+                    if sections[0]:
+                        new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                    new_nodes.append(TextNode(link, TextType.LINK, url))
+                    text = sections[1]
+
+                if text:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from split_nodes import split_nodes_delimiter
+from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_no_delimiter(self):
@@ -61,3 +61,87 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("hello *bitallic?**", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "**", TextType.BOLD)
+
+
+    def test_split_nodes_image_basic(self):
+        node = TextNode("Hello ![test](test.png) world", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(nodes[0].text, "Hello ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "test")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "test.png")
+        self.assertEqual(nodes[2].text, " world")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_multiple_images(self):
+        node = TextNode("Hello ![test](test.png) world ![second](test2.png) and universe", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(nodes[0].text, "Hello ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "test")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "test.png")
+        self.assertEqual(nodes[2].text, " world ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "second")
+        self.assertEqual(nodes[3].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[3].url, "test2.png")
+        self.assertEqual(nodes[4].text, " and universe")
+        self.assertEqual(nodes[4].text_type, TextType.TEXT)
+
+    def test_split_nodes_no_images(self):
+        node = TextNode("Hello world", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(nodes[0].text, "Hello world")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+
+    def test_split_nodes_just_image(self):
+        node = TextNode("![test](test.png)", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(nodes[0].text, "test")
+        self.assertEqual(nodes[0].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[0].url, "test.png")
+
+
+
+
+    def test_split_nodes_link_basic(self):
+        node = TextNode("Hello [test](test.png) world", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(nodes[0].text, "Hello ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "test")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "test.png")
+        self.assertEqual(nodes[2].text, " world")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_multiple_links(self):
+        node = TextNode("Hello [test](test.png) world [second](test2.png) and universe", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(nodes[0].text, "Hello ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "test")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "test.png")
+        self.assertEqual(nodes[2].text, " world ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "second")
+        self.assertEqual(nodes[3].text_type, TextType.LINK)
+        self.assertEqual(nodes[3].url, "test2.png")
+        self.assertEqual(nodes[4].text, " and universe")
+        self.assertEqual(nodes[4].text_type, TextType.TEXT)
+
+    def test_split_nodes_no_links(self):
+        node = TextNode("Hello world", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(nodes[0].text, "Hello world")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+
+    def test_split_nodes_just_link(self):
+        node = TextNode("[test](test.png)", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(nodes[0].text, "test")
+        self.assertEqual(nodes[0].text_type, TextType.LINK)
+        self.assertEqual(nodes[0].url, "test.png")
