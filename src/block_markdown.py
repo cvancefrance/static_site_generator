afd2bfce777c343838f971ee7e4e3309dcd2e29b
cvancefrance <cvancefrance@gmail.com>
2025-01-24 14:01:58 -0600

    added initial block_markdown file

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,1 +1,9 @@
-def
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
